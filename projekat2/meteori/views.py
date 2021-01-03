de61b2334000bb2e75232b5458bbb0849604c758
DraganMicic <30472452+DraganMicic@users.noreply.github.com>
2021-01-03 01:13:36 +0100

    dodato dodavanje

--- projekat2/meteori/views.py
+++ projekat2/meteori/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Meteor
+from .forms import MeteorForm
 
 
 def all_meteors(req):
@@ -10,12 +11,23 @@ def all_meteors(req):
 
 @login_required
 def my_meteors(req):
-    tmp = Meteor.objects.all()
-    return render(req,'my_meteors.html', {'meteors': tmp})
+    tmp = Meteor.objects.filter(posmatrac_id = req.user.id)
+    return render(req, 'my_meteors.html', {'meteors': tmp})
 
 @login_required
 def add_meteor(req):
-    return render(req, 'add_meteors.html')
+    if req.method == 'POST':
+        forma = MeteorForm(req.POST)
+
+        if forma.is_valid():
+            m = Meteor(datum=forma.cleaned_data['datum'], vreme=forma.cleaned_data['vreme'], mesto=forma.cleaned_data['mesto'], magnituda=forma.cleaned_data['magnituda'], posmatrac=req.user)
+            m.save()
+            return redirect('met_app:all_meteors')
+        else:
+            return render(req, 'add_meteors.html', {'form': forma})
+    else:
+        forma = MeteorForm()
+        return render(req, 'add_meteors.html', {'form': forma})
 
 
 def singin(req):
